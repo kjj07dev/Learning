8762eec56de775ed03006be0c78dc9d79169cc9d
kjj07dev <kjj07dev@gmail.com>
2026-09-08 03:08:12 +0000

    .

---
+++ c/week2-3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int maxV, minV;
+    int a = 900, b = 800;
+
+    maxV = (a > b) ? a : b;
+    minV = (a < b) ? a : b;
+
+    printf("900과 800 중에 더 큰 수: %d\n", maxV);
+    printf("900과 800 중에 더 작은 수: %d", minV);
+
+    return 0;
+}
